67573bb9497bc981cedbc558744b8e9aac4885c5
Aniket Kadam <anikadamg@gmail.com>
2019-08-27 21:46:06 +0530

    Implemented code to load the current weather when the vm is created.

--- app/src/main/java/com/aniketkadam/temperatureapp/temperaturedisplay/TemperatureFragmentVm.kt
+++ app/src/main/java/com/aniketkadam/temperatureapp/temperaturedisplay/TemperatureFragmentVm.kt
@@ -4,14 +4,33 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.aniketkadam.temperatureapp.temperaturedisplay.data.WeatherAtLocation
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
-class TemperatureFragmentVm(private val repository: TemperatureDisplayRepository) : ViewModel() {
+class TemperatureFragmentVm(repository: TemperatureDisplayRepository) : ViewModel() {
 
+    private val disposable = CompositeDisposable()
     private val _currentWeather = MutableLiveData<LceWeather>()
+
+    init {
+        disposable.add(
+            repository.getCurrentWeather().subscribeOn(Schedulers.io())
+                .map<LceWeather> { LceWeather.Success(it) }
+                .onErrorReturn { LceWeather.Error(it) }
+                .startWith(LceWeather.Loading)
+                .subscribe {
+                    _currentWeather.postValue(it)
+                }
+        )
+    }
 
     val currentWeather: LiveData<LceWeather>
         get() = _currentWeather
 
+    override fun onCleared() {
+        disposable.dispose()
+        super.onCleared()
+    }
 }
 
 sealed class LceWeather {
