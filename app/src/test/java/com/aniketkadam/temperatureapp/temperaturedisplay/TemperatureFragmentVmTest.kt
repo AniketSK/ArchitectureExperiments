7642229f85a0a8b4e47d0fc631ebcd66ae2e3ab2
Aniket Kadam <anikadamg@gmail.com>
2019-08-27 22:33:19 +0530

    Test that errors are handled when they occur.

--- app/src/test/java/com/aniketkadam/temperatureapp/temperaturedisplay/TemperatureFragmentVmTest.kt
+++ app/src/test/java/com/aniketkadam/temperatureapp/temperaturedisplay/TemperatureFragmentVmTest.kt
@@ -10,13 +10,18 @@ import io.mockk.verify
 import io.reactivex.Observable
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.instanceOf
 import org.junit.Rule
 import org.junit.Test
+import java.net.SocketTimeoutException
 
 class TemperatureFragmentVmTest {
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val rxImmediateSchedulerRule = RxImmediateSchedulerRule()
 
     val repository = mockk<TemperatureDisplayRepository> {
         every { getCurrentWeather() } returns Observable.just(
@@ -47,4 +52,14 @@ class TemperatureFragmentVmTest {
         )
     }
 
+
+    @Test
+    fun `when the vm is created loading is shown first`() {
+        val repository = mockk<TemperatureDisplayRepository> {
+            every { getCurrentWeather() } returns Observable.error(SocketTimeoutException("read timed out"))
+        }
+        val vm = TemperatureFragmentVm(repository)
+
+        assertThat(vm.currentWeather.value, instanceOf(LceWeather.Error::class.java))
+    }
 }
