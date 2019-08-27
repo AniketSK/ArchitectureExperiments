dabebf51d22dc4337c81ed1dca3af8607a3a3829
Aniket Kadam <anikadamg@gmail.com>
2019-08-27 20:43:38 +0530

    Created a weather api to get the location and the current weather.

---
+++ app/src/main/java/com/aniketkadam/temperatureapp/temperaturedisplay/data/WeatherApi.kt
@@ -0,0 +1,19 @@
+package com.aniketkadam.temperatureapp.temperaturedisplay.data
+
+import com.aniketkadam.temperatureapp.BuildConfig
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+
+    @GET("ip.json?q=auto:ip")
+    fun getIpLocation(@Query("key") key: String = BuildConfig.WEATHER_API_KEY): Observable<IpLocationData>
+
+    @GET("current.json")
+    fun getCurrentWeatherAtLocation(
+        @Query("q") city: String,
+        @Query("key") key: String = BuildConfig.WEATHER_API_KEY
+    ): Observable<WeatherAtLocation>
+
+}
