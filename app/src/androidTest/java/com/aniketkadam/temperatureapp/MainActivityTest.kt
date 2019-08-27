f34c8bf61c77bbc9ec5b02d678715847dd48f3d1
Aniket Kadam <anikadamg@gmail.com>
2019-08-27 23:55:11 +0530

    Created an espresso test to check if Mumbai is displayed when it runs.

---
+++ app/src/androidTest/java/com/aniketkadam/temperatureapp/MainActivityTest.kt
@@ -0,0 +1,23 @@
+package com.aniketkadam.temperatureapp
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    val activityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun activity_launches() {
+        onView(withText("Mumbai")).check(matches(isDisplayed()))
+    }
+}
