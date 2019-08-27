d71c22464deb04851ca99eccf7802cb6e785ab81
Aniket Kadam <anikadamg@gmail.com>
2019-08-27 23:09:03 +0530

    Allowing for the construction of vm's for the activity and shared to fragments.

---
+++ app/src/main/java/com/aniketkadam/temperatureapp/di/MainActivityModule.kt
@@ -0,0 +1,31 @@
+package com.aniketkadam.temperatureapp.di
+
+import androidx.lifecycle.ViewModelProviders
+import com.aniketkadam.temperatureapp.MainActivity
+import com.aniketkadam.temperatureapp.temperaturedisplay.TemperatureDisplayRepository
+import com.aniketkadam.temperatureapp.temperaturedisplay.TemperatureFragmentFactory
+import com.aniketkadam.temperatureapp.temperaturedisplay.TemperatureVm
+import dagger.Module
+import dagger.Provides
+import javax.inject.Named
+
+const val MAIN_VM = "MAIN_VM"
+const val FRAGMENT_VM = "FRAGMENT_VM"
+
+@Module
+class MainActivityModule {
+
+    @Named(MAIN_VM)
+    @Provides
+    fun getTemperatureVm(mainActivity: MainActivity, factory: TemperatureFragmentFactory) =
+        ViewModelProviders.of(mainActivity, factory).get(TemperatureVm::class.java)
+
+    @Named(FRAGMENT_VM)
+    @Provides
+    fun getTemperatureVmForFragments(mainActivity: MainActivity) =
+        ViewModelProviders.of(mainActivity).get(TemperatureVm::class.java)
+
+    @Provides
+    fun getTemperatureVmFactory(repository: TemperatureDisplayRepository) =
+        TemperatureFragmentFactory(repository)
+}
