f09b37603dfbf9e9b424e1d5ba00e053251cac8b
Aniket Kadam <anikadamg@gmail.com>
2019-08-27 23:53:17 +0530

    Mainactivity has vm injected.

--- app/src/main/java/com/aniketkadam/temperatureapp/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/temperatureapp/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.aniketkadam.temperatureapp
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.aniketkadam.temperatureapp.di.MAIN_VM
+import com.aniketkadam.temperatureapp.temperaturedisplay.TemperatureVm
+import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
+import javax.inject.Named
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
+
+    @field:Named(MAIN_VM)
+    @Inject
+    lateinit var vm: TemperatureVm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
