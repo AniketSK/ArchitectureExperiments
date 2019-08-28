ed8f488a4f2f5cdc58c610ebf3689aa7d2f2564b
Aniket Kadam <anikadamg@gmail.com>
2019-08-28 11:20:12 +0530

    Create the nav direction from loading to temprature display and show it when the vm says that the current weather loading succeeded.

--- app/src/main/java/com/aniketkadam/temperatureapp/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/temperatureapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.aniketkadam.temperatureapp
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import com.aniketkadam.temperatureapp.di.MAIN_VM
+import com.aniketkadam.temperatureapp.loading.LoadingFragmentDirections
+import com.aniketkadam.temperatureapp.temperaturedisplay.LceWeather
 import com.aniketkadam.temperatureapp.temperaturedisplay.TemperatureVm
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
@@ -16,5 +20,12 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        vm.currentWeather.observe(this, Observer { switchScreen(it) })
+    }
+
+    private fun switchScreen(lceWeather: LceWeather) = with(findNavController(R.id.nav_host)) {
+        when (lceWeather) {
+            is LceWeather.Success -> navigate(LoadingFragmentDirections.actionLoadingFragmentToTemperatureDisplayFragment())
+        }
     }
 }
