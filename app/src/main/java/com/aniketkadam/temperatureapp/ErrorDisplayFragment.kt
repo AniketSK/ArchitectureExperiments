82248e48500cf885079e83ff3d19067228261cef
Aniket Kadam <anikadamg@gmail.com>
2019-08-28 18:21:46 +0530

    Bound the vm to the layout and the layout to the error fragment.

--- app/src/main/java/com/aniketkadam/temperatureapp/ErrorDisplayFragment.kt
+++ app/src/main/java/com/aniketkadam/temperatureapp/ErrorDisplayFragment.kt
@@ -1,5 +1,11 @@
 package com.aniketkadam.temperatureapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.aniketkadam.temperatureapp.databinding.FragmentErrorDisplayBinding
 import com.aniketkadam.temperatureapp.temperaturedisplay.TemperatureVm
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
@@ -9,5 +15,21 @@ class ErrorDisplayFragment : DaggerFragment() {
     @field:Named
     @Inject
     lateinit var vm: TemperatureVm
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentErrorDisplayBinding =
+            DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_error_display,
+                container,
+                false
+            )
+        binding.vm = vm
+        binding.lifecycleOwner = this
+        return binding.root
+    }
 
 }
