cb445e08518a74c989fcfceb637fa2f84042c454
Aniket Kadam <anikadamg@gmail.com>
2019-08-28 21:40:50 +0530

    Planting Timber in the test application.

--- app/src/androidTest/java/com/aniketkadam/temperatureapp/TestApplication.kt
+++ app/src/androidTest/java/com/aniketkadam/temperatureapp/TestApplication.kt
@@ -5,6 +5,7 @@ import androidx.multidex.MultiDex
 import com.aniketkadam.temperatureapp.di.DaggerAppComponentForTest
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import timber.log.Timber
 
 class TestApplication : DaggerApplication() {
 
@@ -15,6 +16,11 @@ class TestApplication : DaggerApplication() {
 
     val appInjector by lazy {
         DaggerAppComponentForTest.builder().application(this).build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> = appInjector
