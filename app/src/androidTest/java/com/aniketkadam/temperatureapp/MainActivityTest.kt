3d4f1e39285c5c0e0555056422fb427e739ea3e9
Aniket Kadam <anikadamg@gmail.com>
2019-08-28 17:18:36 +0530

    Added tests to check that the city and temperature are shown on success.

--- app/src/androidTest/java/com/aniketkadam/temperatureapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/aniketkadam/temperatureapp/MainActivityTest.kt
@@ -1,10 +1,15 @@
 package com.aniketkadam.temperatureapp
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import io.appflate.restmock.RESTMockServer
+import io.appflate.restmock.utils.RequestMatchers.pathEndsWithIgnoringQueryParams
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,10 +18,19 @@ import org.junit.runner.RunWith
 class MainActivityTest {
 
     @get:Rule
-    val activityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+    val activityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java, true, false)
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+        RESTMockServer.reset()
+    }
 
     @Test
     fun loading_is_shown_when_the_app_launches() {
+        activityTestRule.launchActivity(null)
         onView(withContentDescription(R.string.loading_content_description)).check(
             matches(
                 isDisplayed()
@@ -25,7 +39,26 @@ class MainActivityTest {
     }
 
     @Test
-    fun on_success_after_loading_the_temperature_is_shown() {
+    fun on_success_after_loading_the_city_is_shown() {
+        RESTMockServer.whenGET(pathEndsWithIgnoringQueryParams("ip.json"))
+            .thenReturnFile(200, "mocks/api_response_for_ip_location.json")
+        RESTMockServer.whenGET(pathEndsWithIgnoringQueryParams("current.json"))
+            .thenReturnFile(200, "mocks/api_response_for_current.json")
+
+        activityTestRule.launchActivity(null)
         onView(withText("Mumbai")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun on_success_after_loading_the_temperature_is_shown() {
+        RESTMockServer.whenGET(pathEndsWithIgnoringQueryParams("ip.json"))
+            .thenReturnFile(200, "mocks/api_response_for_ip_location.json")
+        RESTMockServer.whenGET(pathEndsWithIgnoringQueryParams("current.json"))
+            .thenReturnFile(200, "mocks/api_response_for_current.json")
+
+        activityTestRule.launchActivity(null)
+        val formattedTemperature = InstrumentationRegistry.getInstrumentation()
+            .context.resources.getString(R.string.degreesFormatting, 30f)
+        onView(withText(formattedTemperature)).check(matches(isDisplayed()))
     }
 }
