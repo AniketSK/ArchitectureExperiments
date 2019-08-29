824d91f1b364b47cd6c73fdfcc645bba82eee100
Aniket Kadam <anikadamg@gmail.com>
2019-08-29 12:19:28 +0530

    Setting the list dividers, cleaned up the code,

--- app/src/main/java/com/aniketkadam/temperatureapp/TemperatureDisplayFragment.kt
+++ app/src/main/java/com/aniketkadam/temperatureapp/TemperatureDisplayFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.aniketkadam.temperatureapp.databinding.FragmentTemperatureDisplayBinding
 import com.aniketkadam.temperatureapp.di.FRAGMENT_VM
 import com.aniketkadam.temperatureapp.temperaturedisplay.TemperatureVm
@@ -15,6 +17,7 @@ import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_temperature_display.*
 import javax.inject.Inject
 import javax.inject.Named
+
 
 class TemperatureDisplayFragment : DaggerFragment() {
 
@@ -36,17 +39,31 @@ class TemperatureDisplayFragment : DaggerFragment() {
         binding.vm = temperatureVm
         binding.lifecycleOwner = this
 
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
         temperatureVm.currentWeatherState.observe(
             viewLifecycleOwner,
             Observer { initAdapter(it?.daysForecast) })
-        return binding.root
+    }
+
+    private fun initRecyclerView() {
+        val mDividerItemDecoration = DividerItemDecoration(
+            upcomingTemperaturesRecyclerView.context,
+            LinearLayoutManager.VERTICAL
+        )
+        upcomingTemperaturesRecyclerView.addItemDecoration(mDividerItemDecoration)
     }
 
     private fun initAdapter(daysForecast: List<FormattedForecastDay>?) {
         if (daysForecast == null) {
             return
         }
-        upcomingTemperaturesListView.adapter = ForecastDayAdapter().apply {
+
+        upcomingTemperaturesRecyclerView.adapter = ForecastDayAdapter().apply {
             submitList(daysForecast.toMutableList())
         }
     }
