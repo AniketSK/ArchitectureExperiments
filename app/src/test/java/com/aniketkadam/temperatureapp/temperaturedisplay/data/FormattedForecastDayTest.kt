89eaef9af8d69ccdad22150ff70597751558a529
Aniket Kadam <anikadamg@gmail.com>
2019-08-29 13:13:51 +0530

    Test for number formatting of forecast object temperatures.

--- app/src/test/java/com/aniketkadam/temperatureapp/temperaturedisplay/data/FormattedForecastDayTest.kt
+++ app/src/test/java/com/aniketkadam/temperatureapp/temperaturedisplay/data/FormattedForecastDayTest.kt
@@ -13,4 +13,21 @@ class FormattedForecastDayTest {
 
         assertThat(underTest.date, equalTo("Monday"))
     }
+
+    @Test
+    fun `the temperature from forecast has a single decimal point if present`() {
+        val sampleForecast = ForecastDay("2019-08-12", 21.2f)
+        val underTest = FormattedForecastDay(sampleForecast)
+
+        assertThat(underTest.temp, equalTo("21.2"))
+    }
+
+    @Test
+    fun `the temperature from forecast has no decimal points at all if it ends with zero`() {
+        val sampleForecast = ForecastDay("2019-08-12", 21.0f)
+        val underTest = FormattedForecastDay(sampleForecast)
+
+        assertThat(underTest.temp, equalTo("21"))
+
+    }
 }
