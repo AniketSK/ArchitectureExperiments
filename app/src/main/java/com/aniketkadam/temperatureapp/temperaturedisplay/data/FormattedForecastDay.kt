7bbb26f483932b74be8c4be77b619db97c3b07fc
Aniket Kadam <anikadamg@gmail.com>
2019-08-29 11:16:31 +0530

    Created a date formatter for the forecast day.

---
+++ app/src/main/java/com/aniketkadam/temperatureapp/temperaturedisplay/data/FormattedForecastDay.kt
@@ -0,0 +1,18 @@
+package com.aniketkadam.temperatureapp.temperaturedisplay.data
+
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+
+
+@SuppressLint("SimpleDateFormat") // The date conversion is unaffected by locale, a date is always the same day no matter which locale you look in.
+data class FormattedForecastDay(private val forecastDay: ForecastDay) {
+
+    val date: String by lazy {
+        val givenDate = SimpleDateFormat("yyyy-MM-dd").parse(forecastDay.date)
+        val formattedDay = SimpleDateFormat("EEEE").format(givenDate)
+        formattedDay
+    }
+
+    val temp
+        get() = forecastDay.temperature
+}
---
+++ app/src/test/java/com/aniketkadam/temperatureapp/temperaturedisplay/data/FormattedForecastDayTest.kt
@@ -0,0 +1,16 @@
+package com.aniketkadam.temperatureapp.temperaturedisplay.data
+
+import org.hamcrest.Matchers.equalTo
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+class FormattedForecastDayTest {
+
+    @Test
+    fun `thee day of week from forecast is correctly formatted`() {
+        val sampleForecast = ForecastDay("2019-08-12", 21f)
+        val underTest = FormattedForecastDay(sampleForecast)
+
+        assertThat(underTest.date, equalTo("Monday"))
+    }
+}
